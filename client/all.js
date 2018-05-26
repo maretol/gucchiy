6ca57969771e41d419419e002b35f3443439a904
Motoharu SOMEYA <maretol@outlook.com>
2018-05-27 00:50:48 +0900

    something

--- client/all.js
+++ client/all.js
@@ -4,13 +4,26 @@ function onClickPost(){
         showNoGuchiMessage()
         return
     }
-    alert(text)
+    showMessageModalDialog("Guchiを受け取ったよ！！")
 }
 
 function showNoGuchiMessage(){
-    alert("愚痴がないのはいいことだよね！！！")
+    showMessageModalDialog("Guchiがないのはいいことだよね！！！")
 }
 
 function showMessageModalDialog(text){
-
+    document.getElementById("resultBody").textContent = text
+    $('#messageModal').modal();
 }
+
+$('#mainTab a').on('show.bs.tab', function(event){
+    if($(event.target).text()=="somegucchi"){
+        for(var i=0; i<10; i++){
+            console.log("hogehoge")
+            var container = document.getElementById("someGuchiList")
+            var item = document.createElement("li")
+            item.textContent = "HELLO"
+            container.appendChild(item);
+        }
+    }
+})
